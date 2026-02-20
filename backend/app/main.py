aa98b8f6947577ed637b2104a2c12a52c96f0508
sudhasrs <sudhasrs26@gmail.com>
2026-02-19 20:06:18 -0800

    Fixed deactivated user , restrictedd from login

--- backend/app/main.py
+++ backend/app/main.py
@@ -130,6 +130,13 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    
+    if not user.active:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Inactive user",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     access_token_expires = timedelta(minutes=auth.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = auth.create_access_token(
         data={"sub": user.username, "role": user.role, "school_id": user.school_id}, expires_delta=access_token_expires
